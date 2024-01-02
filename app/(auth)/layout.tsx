efbbbe6e22406d53dfbf8d7a888a639f1ab9b9c5
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-01-02 12:19:52 +0000

    feat: added darkmode at Clerk authentication and using shadcn-ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
+import { Logo } from "./_components/logo";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="flex flex-col h-full items-center justify-center space-y-6">
+      <Logo />
       {children}
     </div>
   );
