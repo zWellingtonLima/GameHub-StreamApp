51b4fd35bdbb28c531f9d49c71ce24485acbd60d
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-01-16 13:11:43 +0000

    feat: added major recommended list components

--- app/(browse)/_components/sidebar/user-item.tsx
+++ app/(browse)/_components/sidebar/user-item.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+import { useSidebar } from "@/store/use-sidebar";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { UserAvatar } from "@/components/user-avatar";
+import { LiveBadge } from "@/components/live-badge";
+import { Skeleton } from "@/components/ui/skeleton";
 interface UserItemProps {
   username: string;
   imageUrl: string;
@@ -7,5 +16,46 @@ interface UserItemProps {
 }
 
 export const UserItem = ({ username, imageUrl, isLive }: UserItemProps) => {
-  return <div>Useritem</div>;
+  const pathname = usePathname();
+
+  const { collapsed } = useSidebar((state) => state);
+
+  const href = `/${username}`;
+  const isActive = pathname === href;
+
+  return (
+    <Button
+      asChild
+      variant="ghost"
+      className={cn(
+        "w-full h-1/2",
+        collapsed ? "justify-center" : "justify-start",
+        isActive && "bg-accent"
+      )}
+    >
+      <Link href={href}>
+        <div
+          className={cn(
+            "flex items-center w-full gap-x-4",
+            collapsed && "justify-center"
+          )}
+        >
+          <UserAvatar imageUrl={imageUrl} username={username} isLive={isLive} />
+          {!collapsed && <p className="truncate">{username}</p>}
+          {!collapsed && isLive && <LiveBadge className="ml-auto" />}
+        </div>
+      </Link>
+    </Button>
+  );
+};
+
+export const UserItemSkeleton = () => {
+  return (
+    <li className="flex items-center gap-x-4 px-3 py-2">
+      <Skeleton className="min-h-[32px] min-w-[32px] rounded-full" />
+      <div className="flex-1">
+        <Skeleton className="h-6" />
+      </div>
+    </li>
+  );
 };
--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,16 +1,18 @@
+import { Suspense } from "react";
+
 import { Container } from "./_components/container";
 import { Navbar } from "./_components/navbar";
-import { Sidebar } from "./_components/sidebar";
+import { Sidebar, SidebarSkeleton } from "./_components/sidebar";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
-      <div className='flex h-full pt-20'>
-        <Sidebar/>
-        <Container>
-          {children}
-        </Container>
+      <div className="flex h-full pt-20">
+        <Suspense fallback={<SidebarSkeleton />}>
+          <Sidebar />
+        </Suspense>
+        <Container>{children}</Container>
       </div>
     </>
   );
