b498337738c058333625016b5e6efe1ce7c790e2
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-02-03 13:02:16 +0000

    feat: added block and unblock at block-services and included filter the users at follow-service and recommended-service as well.

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -23,8 +23,8 @@ export const isBlockedByUser = async (id: string) => {
       where: {
         blockerId_blockedId: {
           blockerId: otherUser.id,
-          blockedId: self.id
-        }
+          blockedId: self.id,
+        },
       },
     });
 
@@ -32,4 +32,89 @@ export const isBlockedByUser = async (id: string) => {
   } catch (error) {
     return false;
   }
+};
+
+export const blockUser = async (id: string) => {
+  const self = await getSelf();
+
+  if (self.id === id) {
+    throw new Error("Cannot block yourself.");
+  }
+
+  const otherUser = await db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!otherUser) {
+    throw new Error("User not found.");
+  }
+
+  const existingBlock = await db.block.findUnique({
+    where: {
+      blockerId_blockedId: {
+        blockerId: self.id,
+        blockedId: otherUser.id,
+      },
+    },
+  });
+
+  if (existingBlock) {
+    throw new Error("Already blocked");
+  }
+
+  const block = await db.block.create({
+    data: {
+      blockerId: self.id,
+      blockedId: otherUser.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return block;
+};
+
+export const unblockUser = async (id: string) => {
+  const self = await getSelf();
+
+  if (self.id === id) {
+    throw new Error("Cannot unblock yourself.");
+  }
+
+  const otherUser = await db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!otherUser) {
+    throw new Error("User not found.");
+  }
+
+  const existingBlock = await db.block.findUnique({
+    where: {
+      blockerId_blockedId: {
+        blockerId: self.id,
+        blockedId: otherUser.id,
+      },
+    },
+  });
+
+  if (!existingBlock) {
+    throw new Error("Not blocked.");
+  }
+
+  const unblock = await db.block.delete({
+    where: {
+      id: existingBlock.id,
+    },
+    include: {
+      blocked: true,
+    },
+  });
+
+  return unblock;
 };
